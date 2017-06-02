377134fb967c9f2f5237d5a11d4da8d85f21493a
winenot <ksusha_9410@mail.ru>
2017-06-02 11:04:22 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Документ без названия</title>
+</head>
+
+<body>
+Лето солнце море песок<img src="citati_o_lete_3.jpg" width="600" height="400" longdesc="citati_o_lete_3.jpg" />
+</body>
+</html>
